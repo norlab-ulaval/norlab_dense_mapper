579184608c32f4ed9b67bad07c5b7a81d170381d
Alexandre Guénette <alexandre.guenette.1@ulaval.ca>
2021-05-17 13:32:03 -0400

    Refactoring of the norlab_icp_mapper library to make a dense mapper

--- norlab_dense_mapper/DenseMapper.cpp
+++ norlab_dense_mapper/DenseMapper.cpp
@@ -2,190 +2,202 @@
 #include <fstream>
 #include <chrono>
 
-norlab_dense_mapper::DenseMapper::DenseMapper(const std::string& inputFiltersConfigFilePath, const std::string& icpConfigFilePath,
-								  const std::string& mapPostFiltersConfigFilePath, const std::string& mapUpdateCondition, const float& mapUpdateOverlap,
-								  const float& mapUpdateDelay, const float& mapUpdateDistance, const float& minDistNewPoint, const float& sensorMaxRange,
-								  const float& priorDynamic, const float& thresholdDynamic, const float& beamHalfAngle, const float& epsilonA,
-								  const float& epsilonD, const float& alpha, const float& beta, const bool& is3D, const bool& isOnline,
-								  const bool& computeProbDynamic, const bool& isMapping, const bool& saveMapCellsOnHardDrive):
-		mapUpdateCondition(mapUpdateCondition),
-		mapUpdateOverlap(mapUpdateOverlap),
-		mapUpdateDelay(mapUpdateDelay),
-		mapUpdateDistance(mapUpdateDistance),
-		is3D(is3D),
-		isOnline(isOnline),
-		isMapping(isMapping),
-		map(minDistNewPoint, sensorMaxRange, priorDynamic, thresholdDynamic, beamHalfAngle, epsilonA, epsilonD, alpha, beta, is3D,
-			isOnline, computeProbDynamic, saveMapCellsOnHardDrive, icp, icpMapLock),
-		trajectory(is3D ? 3 : 2),
-		transformation(PM::get().TransformationRegistrar.create("RigidTransformation"))
+norlab_dense_mapper::DenseMapper::DenseMapper(const std::string& inputFiltersConfigFilePath,
+                                              const std::string& mapPostFiltersConfigFilePath,
+                                              std::string mapUpdateCondition,
+                                              const float& mapUpdateDelay,
+                                              const float& mapUpdateDistance,
+                                              const float& minDistNewPoint,
+                                              const float& sensorMaxRange,
+                                              const float& priorDynamic,
+                                              const float& thresholdDynamic,
+                                              const float& beamHalfAngle,
+                                              const float& epsilonA,
+                                              const float& epsilonD,
+                                              const float& alpha,
+                                              const float& beta,
+                                              const bool& is3D,
+                                              const bool& isOnline,
+                                              const bool& computeProbDynamic,
+                                              const bool& isMapping,
+                                              const bool& saveMapCellsOnHardDrive) :
+    mapUpdateCondition(std::move(mapUpdateCondition)),
+    mapUpdateDelay(mapUpdateDelay),
+    mapUpdateDistance(mapUpdateDistance),
+    is3D(is3D),
+    isOnline(isOnline),
+    isMapping(isMapping),
+    denseMap(minDistNewPoint,
+             sensorMaxRange,
+             priorDynamic,
+             thresholdDynamic,
+             beamHalfAngle,
+             epsilonA,
+             epsilonD,
+             alpha,
+             beta,
+             is3D,
+             isOnline,
+             computeProbDynamic,
+             saveMapCellsOnHardDrive),
+    trajectory(is3D ? 3 : 2),
+    transformation(PM::get().TransformationRegistrar.create("RigidTransformation"))
 {
-	loadYamlConfig(inputFiltersConfigFilePath, icpConfigFilePath, mapPostFiltersConfigFilePath);
+    loadYamlConfig(inputFiltersConfigFilePath, mapPostFiltersConfigFilePath);
 
-	PM::Parameters radiusFilterParams;
-	radiusFilterParams["dim"] = "-1";
-	radiusFilterParams["dist"] = std::to_string(sensorMaxRange);
-	radiusFilterParams["removeInside"] = "0";
-	radiusFilter = PM::get().DataPointsFilterRegistrar.create("DistanceLimitDataPointsFilter", radiusFilterParams);
+    PM::Parameters radiusFilterParams;
+    radiusFilterParams["dim"] = "-1";
+    radiusFilterParams["dist"] = std::to_string(sensorMaxRange);
+    radiusFilterParams["removeInside"] = "0";
+    radiusFilter = PM::get().DataPointsFilterRegistrar.create("DistanceLimitDataPointsFilter",
+                                                              radiusFilterParams);
 }
 
-void norlab_dense_mapper::DenseMapper::loadYamlConfig(const std::string& inputFiltersConfigFilePath, const std::string& icpConfigFilePath,
-											   const std::string& mapPostFiltersConfigFilePath)
+void norlab_dense_mapper::DenseMapper::loadYamlConfig(
+    const std::string& inputFiltersConfigFilePath,
+    const std::string& mapPostFiltersConfigFilePath)
 {
-	if(!icpConfigFilePath.empty())
-	{
-		std::ifstream ifs(icpConfigFilePath.c_str());
-		icp.loadFromYaml(ifs);
-		ifs.close();
-	}
-	else
-	{
-		icp.setDefault();
-	}
+    if (!inputFiltersConfigFilePath.empty())
+    {
+        std::ifstream ifs(inputFiltersConfigFilePath.c_str());
+        inputFilters = PM::DataPointsFilters(ifs);
+        ifs.close();
+    }
 
-	if(!inputFiltersConfigFilePath.empty())
-	{
-		std::ifstream ifs(inputFiltersConfigFilePath.c_str());
-		inputFilters = PM::DataPointsFilters(ifs);
-		ifs.close();
-	}
-
-	if(!mapPostFiltersConfigFilePath.empty())
-	{
-		std::ifstream ifs(mapPostFiltersConfigFilePath.c_str());
-		mapPostFilters = PM::DataPointsFilters(ifs);
-		ifs.close();
-	}
+    if (!mapPostFiltersConfigFilePath.empty())
+    {
+        std::ifstream ifs(mapPostFiltersConfigFilePath.c_str());
+        mapPostFilters = PM::DataPointsFilters(ifs);
+        ifs.close();
+    }
 }
 
-void norlab_dense_mapper::DenseMapper::processInput(const PM::DataPoints& inputInSensorFrame, const PM::TransformationParameters& estimatedPose,
-											 const std::chrono::time_point<std::chrono::steady_clock>& timeStamp)
+void norlab_dense_mapper::DenseMapper::processInput(
+    const PM::DataPoints& inputInSensorFrame,
+    const PM::TransformationParameters& currentPose,
+    const std::chrono::time_point<std::chrono::steady_clock>& timeStamp)
 {
-	PM::DataPoints filteredInputInSensorFrame = radiusFilter->filter(inputInSensorFrame);
-	inputFilters.apply(filteredInputInSensorFrame);
-	PM::DataPoints input = transformation->compute(filteredInputInSensorFrame, estimatedPose);
+    PM::DataPoints filteredInputInSensorFrame = radiusFilter->filter(inputInSensorFrame);
+    inputFilters.apply(filteredInputInSensorFrame);
+    PM::DataPoints input = transformation->compute(filteredInputInSensorFrame, currentPose);
 
-	PM::TransformationParameters correctedPose;
-	if(map.isLocalPointCloudEmpty())
-	{
-		correctedPose = estimatedPose;
+    if (denseMap.isLocalPointCloudEmpty())
+    {
+        // denseMap.updatePose(currentPose);
+        updateMap(input, currentPose, timeStamp);
+    }
+    else
+    {
+        // denseMap.updatePose(currentPose);
 
-		map.updatePose(correctedPose);
+        if (shouldUpdateMap(timeStamp, currentPose))
+        {
+            updateMap(input, currentPose, timeStamp);
+        }
+    }
 
-		updateMap(input, correctedPose, timeStamp);
-	}
-	else
-	{
-		icpMapLock.lock();
-		PM::TransformationParameters correction = icp(input);
-		icpMapLock.unlock();
+    poseLock.lock();
+    pose = currentPose;
+    poseLock.unlock();
 
-		correctedPose = correction * estimatedPose;
-
-		map.updatePose(correctedPose);
-
-		if(shouldUpdateMap(timeStamp, correctedPose, icp.errorMinimizer->getOverlap()))
-		{
-			updateMap(transformation->compute(input, correction), correctedPose, timeStamp);
-		}
-	}
-
-	poseLock.lock();
-	pose = correctedPose;
-	poseLock.unlock();
-
-	int euclideanDim = is3D ? 3 : 2;
-	trajectoryLock.lock();
-	trajectory.addPoint(correctedPose.topRightCorner(euclideanDim, 1));
-	trajectoryLock.unlock();
+    int euclideanDim = is3D ? 3 : 2;
+    trajectoryLock.lock();
+    trajectory.addPoint(currentPose.topRightCorner(euclideanDim, 1));
+    trajectoryLock.unlock();
 }
 
-bool norlab_dense_mapper::DenseMapper::shouldUpdateMap(const std::chrono::time_point<std::chrono::steady_clock>& currentTime,
-												const PM::TransformationParameters& currentPose, const float& currentOverlap) const
+bool norlab_dense_mapper::DenseMapper::shouldUpdateMap(
+    const std::chrono::time_point<std::chrono::steady_clock>& currentTime,
+    const PM::TransformationParameters& currentPose) const
 {
-	if(!isMapping.load())
-	{
-		return false;
-	}
+    if (!isMapping.load())
+    {
+        return false;
+    }
 
-	if(isOnline)
-	{
-		// if previous update is not over
-		if(mapUpdateFuture.valid() && mapUpdateFuture.wait_for(std::chrono::milliseconds(0)) != std::future_status::ready)
-		{
-			return false;
-		}
-	}
+    if (isOnline)
+    {
+        // if previous update is not over
+        if (mapUpdateFuture.valid() &&
+            mapUpdateFuture.wait_for(std::chrono::milliseconds(0)) != std::future_status::ready)
+        {
+            return false;
+        }
+    }
 
-	if(mapUpdateCondition == "overlap")
-	{
-		return currentOverlap < mapUpdateOverlap;
-	}
-	else if(mapUpdateCondition == "delay")
-	{
-		return (currentTime - lastTimeMapWasUpdated) > std::chrono::duration<float>(mapUpdateDelay);
-	}
-	else
-	{
-		int euclideanDim = is3D ? 3 : 2;
-		PM::Vector lastLocation = lastPoseWhereMapWasUpdated.topRightCorner(euclideanDim, 1);
-		PM::Vector currentLocation = currentPose.topRightCorner(euclideanDim, 1);
-		return std::fabs((currentLocation - lastLocation).norm()) > mapUpdateDistance;
-	}
+    if (mapUpdateCondition == "delay")
+    {
+        return (currentTime - lastTimeMapWasUpdated) > std::chrono::duration<float>(mapUpdateDelay);
+    }
+    else
+    {
+        int euclideanDim = is3D ? 3 : 2;
+        PM::Vector lastLocation = lastPoseWhereMapWasUpdated.topRightCorner(euclideanDim, 1);
+        PM::Vector currentLocation = currentPose.topRightCorner(euclideanDim, 1);
+        return std::fabs((currentLocation - lastLocation).norm()) > mapUpdateDistance;
+    }
 }
 
-void norlab_dense_mapper::DenseMapper::updateMap(const PM::DataPoints& currentInput, const PM::TransformationParameters& currentPose,
-										  const std::chrono::time_point<std::chrono::steady_clock>& currentTimeStamp)
+void norlab_dense_mapper::DenseMapper::updateMap(
+    const PM::DataPoints& currentInput,
+    const PM::TransformationParameters& currentPose,
+    const std::chrono::time_point<std::chrono::steady_clock>& currentTimeStamp)
 {
-	lastTimeMapWasUpdated = currentTimeStamp;
-	lastPoseWhereMapWasUpdated = currentPose;
+    lastTimeMapWasUpdated = currentTimeStamp;
+    lastPoseWhereMapWasUpdated = currentPose;
 
-	if(isOnline && !map.isLocalPointCloudEmpty())
-	{
-		mapUpdateFuture = std::async(std::launch::async, &DenseMap::updateLocalPointCloud, &map, currentInput, currentPose, mapPostFilters);
-	}
-	else
-	{
-		map.updateLocalPointCloud(currentInput, currentPose, mapPostFilters);
-	}
+    if (isOnline && !denseMap.isLocalPointCloudEmpty())
+    {
+        mapUpdateFuture = std::async(std::launch::async,
+                                     &DenseMap::updateLocalPointCloud,
+                                     &denseMap,
+                                     currentInput,
+                                     currentPose,
+                                     mapPostFilters);
+    }
+    else
+    {
+        denseMap.updateLocalPointCloud(currentInput, currentPose, mapPostFilters);
+    }
 }
 
 norlab_dense_mapper::DenseMapper::PM::DataPoints norlab_dense_mapper::DenseMapper::getMap()
 {
-	return map.getGlobalPointCloud();
+    return denseMap.getGlobalPointCloud();
 }
 
 void norlab_dense_mapper::DenseMapper::setMap(const PM::DataPoints& newMap)
 {
-	map.setGlobalPointCloud(newMap);
-	trajectoryLock.lock();
-	trajectory.clearPoints();
-	trajectoryLock.unlock();
+    denseMap.setGlobalPointCloud(newMap);
+    trajectoryLock.lock();
+    trajectory.clearPoints();
+    trajectoryLock.unlock();
 }
 
 bool norlab_dense_mapper::DenseMapper::getNewLocalMap(PM::DataPoints& mapOut)
 {
-	return map.getNewLocalPointCloud(mapOut);
+    return denseMap.getNewLocalPointCloud(mapOut);
 }
 
-norlab_dense_mapper::DenseMapper::PM::TransformationParameters norlab_dense_mapper::DenseMapper::getPose()
+norlab_dense_mapper::DenseMapper::PM::TransformationParameters
+norlab_dense_mapper::DenseMapper::getPose()
 {
-	std::lock_guard<std::mutex> lock(poseLock);
-	return pose;
+    std::lock_guard<std::mutex> lock(poseLock);
+    return pose;
 }
 
 bool norlab_dense_mapper::DenseMapper::getIsMapping() const
 {
-	return isMapping.load();
+    return isMapping.load();
 }
 
 void norlab_dense_mapper::DenseMapper::setIsMapping(const bool& newIsMapping)
 {
-	isMapping.store(newIsMapping);
+    isMapping.store(newIsMapping);
 }
 
 Trajectory norlab_dense_mapper::DenseMapper::getTrajectory()
 {
-	std::lock_guard<std::mutex> lock(trajectoryLock);
-	return trajectory;
+    std::lock_guard<std::mutex> lock(trajectoryLock);
+    return trajectory;
 }
